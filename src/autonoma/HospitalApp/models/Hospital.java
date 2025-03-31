f5f83e3fe56a62efc2eced7a3fce876af774709b
Juan diego Montoya <solismontoya67@mail.com>
2025-03-31 12:06:16 -0500

    First commit

---
+++ src/autonoma/HospitalApp/models/Hospital.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.HospitalApp.models;
+
+/**
+ *
+ * @author solis
+ */
+public class Hospital {
+    
+    
+}
